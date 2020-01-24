5df34630f7ca78a6d8b8e0e9377e3c8460bdb2d5
Allan Legemaate <alegemaate@gmail.com>
2020-01-23 23:19:27 -0500

    Linter works

---
+++ README.md
@@ -0,0 +1,24 @@
+# SublimeLinter-contrib-elm
+
+[![Build Status](https://github.com/SublimeLinter/SublimeLinter-flow/workflows/flake8/badge.svg)
+
+This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides linting functionality for the [Elm language](http://elm-lang.org).
+
+It will be used with files that have the elm syntax package, which can be installed from [here](http://github.com/deadfoxygrandpa/Elm.tmLanguage).
+
+## Installation
+
+SublimeLinter must be installed in order to use this plugin.
+
+Please use [Package Control](https://packagecontrol.io) to install the linter plugin.
+
+[Getting started with Elm](https://guide.elm-lang.org/install/)
+
+Please make sure that the path to `elm` is available to SublimeLinter.
+The docs cover [troubleshooting PATH configuration](http://sublimelinter.com/en/latest/troubleshooting.html#finding-a-linter-executable).
+
+## Settings
+
+- SublimeLinter settings: http://sublimelinter.com/en/latest/settings.html
+- Linter settings: http://sublimelinter.com/en/latest/linter_settings.html
+- SublimeLinter-contrib-elm does not have any special settings.
---
+++ linter.py
@@ -0,0 +1,89 @@
+#
+# linter.py
+# Elm Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Allan Legemaate
+# Github: @alegemaate
+#
+# License: MIT
+#
+
+"""This module exports the Elm plugin class."""
+
+import json
+import logging
+import functools
+
+from SublimeLinter.lint import Linter, util
+
+LOGGER = logging.getLogger('SublimeLinter.plugin.elm')
+
+class Elm(Linter):
+    """Provides an interface to elm make linting."""
+
+    cmd = 'elm make --report=json --output=/dev/null ${temp_file}'
+    regex = (r'^@type=(?P<type>.*?)@@@@line=(?P<line>.*?)@@@@col=(?P<col>.*?)@@@@message=(?P<message>[\s\S]*?)@@@$')
+    multiline = True
+    tempfile_suffix = 'elm'
+    error_stream = util.STREAM_BOTH
+    word_re = None
+    defaults = {
+        'selector': 'source.elm'
+    }
+
+    def run(self, cmd, code):
+        ''' Run elm make command and parse output'''
+        cmd_output = super().run(cmd, code)
+        json_output = None
+
+        # Parse json output, doesn't exist? We can assume clean output
+        try:
+            json_output = json.loads(cmd_output)
+        except ValueError:
+            return None
+
+        # Reduce errors
+        errors = json_output['errors']
+        LOGGER.warning(functools.reduce(lambda a, b: a + "\n" + self.reduce_errors(b), errors, ""))
+        return functools.reduce(lambda a, b: a + "\n" + self.reduce_errors(b), errors, "")
+
+    def reduce_errors(self, json_errors):
+        ''' Reduce array of errors to be sublime friendly '''
+        problems = json_errors['problems']
+        return functools.reduce(lambda a, b: a + "\n" + self.reduce_problems(b), problems, "")
+
+    def reduce_problems(self, error):
+        ''' Pads string so they will match regex '''
+        region = error.get('subregion') or error.get('region')
+
+        column = ''
+        highlight = ''
+        range_length = region['end']['column'] - region['start']['column']
+        if range_length > 0:
+            column = str(region['start']['column'])
+            highlight = "x" * (region['end']['column'] - region['start']['column'])
+
+        def pad_string(name, value):
+            ''' Pads string so they will match regex '''
+            return "@{a}={b}@@@".format(a=name, b=value)
+
+        return "".join([
+            pad_string("type", "error"),
+            pad_string("line", str(region['start']['line'])),
+            pad_string("col", column),
+            pad_string("region", highlight),
+            pad_string("message", self.reduce_message(error)),
+        ])
+
+    def reduce_message(self, json_error):
+        ''' Reduce messages to a single message '''
+
+        def resolve_message(message):
+            '''
+            Resolves a message type to a string, as elm make returns objects 
+            for formatted strings and plain strings for unformatted ones
+            '''
+            return message if isinstance(message, str) else message['string']
+
+        message = functools.reduce(lambda a, b: a + resolve_message(b), json_error['message'], "")
+        return "[{a}]\n{b}".format(a=json_error['title'], b=message)
